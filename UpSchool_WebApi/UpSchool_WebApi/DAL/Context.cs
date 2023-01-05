255b80f92517517590beca7e6bbfe207c1c43914
Serap BERAN <79447748+serapberan@users.noreply.github.com>
2023-01-05 19:50:46 +0300

    WebApi

---
+++ UpSchool_WebApi/UpSchool_WebApi/DAL/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UpSchool_WebApi.DAL.Entities;
+
+namespace UpSchool_WebApi.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-FEC73RP; Database=DBApiUpSchool; integrated security=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ UpSchool_WebApi/UpSchool_WebApi/DAL/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UpSchool_WebApi.DAL.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+    }
+}
